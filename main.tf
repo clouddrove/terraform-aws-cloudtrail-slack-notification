af892e90948d05853162fe335913c56cd2b1ba70
mamraj yadav <themamraj0131@gmail.com>
2023-05-17 13:16:09 +0530

    bug: updated terraform modules versions and github action

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "null_resource" "main" {
 #Description : This terraform module is designed to create cloudtrail log.
 module "cloudtrail-slack" {
   source  = "clouddrove/lambda/aws"
-  version = "0.15.0"
+  version = "1.3.0"
 
   name        = var.name
   repository  = var.repository
