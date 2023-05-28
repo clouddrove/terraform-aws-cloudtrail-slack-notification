98eb553309fc2a770289181f065175c54065a4f3
mamraj yadav <80300065+mamrajyadav@users.noreply.github.com>
2023-05-28 18:39:00 +0530

    bug: updated naming convention (#12)

--- _example/example.tf
+++ _example/example.tf
@@ -25,11 +25,11 @@ module "cloudtrail-slack-notification" {
   bucket_arn  = module.s3_bucket.arn
   bucket_name = module.s3_bucket.id
   variables = {
-    SLACK_WEBHOOK     = "https://hooks.slack.com/services/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"
-    SLACK_CHANNEL     = "testing"
-    EVENT_IGNORE_LIST = jsonencode(["^Describe*", "^Assume*", "^List*", "^Get*", "^Decrypt*", "^Lookup*", "^BatchGet*", "^CreateLogStream$", "^RenewRole$", "^REST.GET.OBJECT_LOCK_CONFIGURATION$", "TestEventPattern", "TestScheduleExpression", "CreateNetworkInterface", "ValidateTemplate"])
-    EVENT_ALERT_LIST  = jsonencode(["DetachRolePolicy", "ConsoleLogin"])
-    USER_IGNORE_LIST  = jsonencode(["^awslambda_*", "^aws-batch$", "^bamboo*", "^i-*", "^[0-9]*$", "^ecs-service-scheduler$", "^AutoScaling$", "^AWSCloudFormation$", "^CloudTrailBot$", "^SLRManagement$"])
-    SOURCE_LIST       = jsonencode(["signin.amazonaws.com"])
+    slack_webhook     = "https://hooks.slack.com/services/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"
+    slack_channel     = "testing"
+    event_ignore_list = jsonencode(["^Describe*", "^Assume*", "^List*", "^Get*", "^Decrypt*", "^Lookup*", "^BatchGet*", "^CreateLogStream$", "^RenewRole$", "^REST.GET.OBJECT_LOCK_CONFIGURATION$", "TestEventPattern", "TestScheduleExpression", "CreateNetworkInterface", "ValidateTemplate"])
+    event_alert_list  = jsonencode(["DetachRolePolicy", "ConsoleLogin"])
+    user_ignore_list  = jsonencode(["^awslambda_*", "^aws-batch$", "^bamboo*", "^i-*", "^[0-9]*$", "^ecs-service-scheduler$", "^AutoScaling$", "^AWSCloudFormation$", "^CloudTrailBot$", "^SLRManagement$"])
+    source_list       = jsonencode(["signin.amazonaws.com"])
   }
 }
