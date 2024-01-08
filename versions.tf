32baa05aceebb541296c22d9207d2444d8edfaaa
anmolnagpal <anmol@clouddrove.com>
2024-01-08 16:52:02 +0530

    feat: update code and fix tf-check issue

--- versions.tf
+++ versions.tf
@@ -1,11 +1,15 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.5.5"
+  required_version = ">= 1.6.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.48.0"
+      version = ">= 5.31.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.2"
     }
   }
 }
